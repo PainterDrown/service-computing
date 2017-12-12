3104ee106f31caee8495f188cbd903991f721383
painterdrown <painterdrown@hotmail.com>
2017-12-12 22:24:57 +0800

    agenda entity 完成

--- agenda/entity/init.go
+++ agenda/entity/init.go
@@ -1,32 +1,27 @@
 package entity
 
 import (
-	"errors"
 	"database/sql"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
 var db *sql.DB
-var createUserTable = `
-CREATE TABLE IF NOT EXISTS user (
-	username VARCHAR(32),
-	password VARCHAR(32)
-);
-`
-var createMeetingTable = `
-CREATE TABLE IF NOT EXISTS meeting (
-	title VARCHAR(32),
-	sponsor VARCHAR(32),
-	participants TEXT,
-	stime CHAR(16)
-	etime CHAR(16)
-);
-`
-
 
 func init() {
 	var err error
 	db, err = sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		panic("数据库连接错误")
+	}
+	_, err = db.Exec(createUserTable)
+	checkErr(err)
+	_, err = db.Exec(createMeetingTable)
+	checkErr(err)
+}
 
-
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
